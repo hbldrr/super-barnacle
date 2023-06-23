88d5faaa2ee19a2430f9467b4f37a96845914e5f
Pawel Ziolkowski <pxziolkowski@gmail.com>
2023-06-23 09:44:48 +0200

    add Main class

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    private String name;
+    private String surname;
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println("Rebase.");
+        System.out.println("main branch");
+        System.out.println("new-branch");
+
+
+        System.out.println("stash 2");
+        System.out.println("stash 1");
+        System.out.println("IntelliJ");
+        System.out.println("stash from here");
+
+    }
+}
