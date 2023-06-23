39a19980ada39f3d5c5a53ae41f676ba66d93128
Pawel Ziolkowski <pxziolkowski@gmail.com>
2023-06-23 13:54:59 +0200

    main brach

---
+++ JKClass.java
@@ -0,0 +1,6 @@
+public class JKClass {
+
+    private String name;
+    private String surname;
+    private String pesel;
+}
---
+++ PZClass.java
@@ -0,0 +1,5 @@
+public class PZClass {
+
+    private String name;
+
+}
